13a8a2425b0691cc82d25adeba288dc8af95e6ca
Damini Varu <damvaru4@gmail.com>
2019-09-22 23:36:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   
   attr_accessor :total, :discount, :last_trans
   
-  def initialize(total, discount, last_trans)
+  def initialize(discount, last_trans)
     @total = total 
     @discount = discount 
     @last_trans = last_trans
